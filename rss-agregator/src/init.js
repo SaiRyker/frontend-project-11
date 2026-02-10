0421f4d1612399d984e55a51ba11ec0a37a961d0
DanilChernyshov <daniilchernyshov0@gmail.com>
2026-02-10 23:23:13 +0300

    added view file

--- rss-agregator/src/init.js
+++ rss-agregator/src/init.js
@@ -4,9 +4,51 @@ import './style.css';
 import { proxy, snapshot, subscribe } from 'valtio/vanilla';
 import { validateForm, validateDublicate } from './validation.js';
 
-export default () => {
+const addStyles = (elements, state) => {
+  const obj = snapshot(state);
+  elements.inputEl.id = 'url-input';
+  elements.inputEl.type = 'text';
+  elements.inputEl.name = 'url';
+  elements.inputEl.placeholder = 'ссылка RSS';
+  elements.inputEl.autocomplete = 'off';
+  elements.inputEl.setAttribute('aria-label', 'url');
+  elements.inputEl.required = true;
+  elements.inputEl.autofocus = true;
+  elements.inputEl.classList.remove('is-invalid');
+
+  if (obj.rssProcess.stateProcess === 'failed') {
+    elements.inputEl.classList.add('is-invalid');
+  }
+
+  elements.submitBtn.type = 'submit';
+  elements.submitBtn.textContent = 'Добавить';
+}
+
+const render = (elements, state) => {
+  console.log(state.feeds);
+  elements.container.innerHTML = '';
+  elements.feeds.innerHTML = '';
+
+  addStyles(elements, state);
+  state.feeds.forEach((feed) => {
+    const h2El = document.createElement('h2');
+    h2El.textContent = 'New Feed';
+    const pEl = document.createElement('p');
+    pEl.textContent = feed;
+    elements.feeds.appendChild(h2El);
+    elements.feeds.appendChild(pEl);
+  });
+  elements.inputEl.value = '';
+  elements.formEl.appendChild(elements.inputEl);
+  elements.formEl.appendChild(elements.submitBtn);
+  elements.container.appendChild(elements.formEl);
+  elements.container.appendChild(elements.feeds);
+  // elements.inputEl.focus()
+}
+
+const init = () => {
   const state = proxy({
-    
+
     formUrl: {
       url: '',
       errors: [],
@@ -28,48 +70,6 @@ export default () => {
     submitBtn: document.createElement('button'),
     feeds: document.createElement('div'),
   };
-
-  function addStyles() {
-    const obj = snapshot(state);
-    elements.inputEl.id = 'url-input';
-    elements.inputEl.type = 'text';
-    elements.inputEl.name = 'url';
-    elements.inputEl.placeholder = 'ссылка RSS';
-    elements.inputEl.autocomplete = 'off';
-    elements.inputEl.setAttribute('aria-label', 'url');
-    elements.inputEl.required = true;
-    elements.inputEl.autofocus = true;
-    elements.inputEl.classList.remove('is-invalid');
-
-    if (obj.rssProcess.stateProcess === 'failed') {
-      elements.inputEl.classList.add('is-invalid');
-    }
-
-    elements.submitBtn.type = 'submit';
-    elements.submitBtn.textContent = 'Добавить';
-  }
-
-  function render() {
-    console.log(state.feeds);
-    elements.container.innerHTML = '';
-    elements.feeds.innerHTML = '';
-
-    addStyles();
-    state.feeds.forEach((feed) => {
-      const h2El = document.createElement('h2');
-      h2El.textContent = 'New Feed';
-      const pEl = document.createElement('p');
-      pEl.textContent = feed;
-      elements.feeds.appendChild(h2El);
-      elements.feeds.appendChild(pEl);
-    });
-    elements.inputEl.value = '';
-    elements.formEl.appendChild(elements.inputEl);
-    elements.formEl.appendChild(elements.submitBtn);
-    elements.container.appendChild(elements.formEl);
-    elements.container.appendChild(elements.feeds);
-    // elements.inputEl.focus()
-  }
 
   subscribe(state, render);
 
@@ -109,5 +109,8 @@ export default () => {
       })
   });
 
-  render();
+  render(elements, state);
 };
+
+
+export {init, render}
---
+++ rss-agregator/src/view.js
@@ -0,0 +1,4 @@
+/* eslint-disable */
+import { render } from "./init.js";
+import onChange from 'on-change';
+
